9074f60f30352ecc9f24571a8a45ad62f58194fd
razanfawwaz <razanfawwaz1905@gmail.com>
2024-06-16 01:45:59 +0700

    feat: init project

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Deployment Success! Hello World!"))
+	})
+
+	log.Println("Server started on port 8080")
+	http.ListenAndServe(":8080", r)
+}
